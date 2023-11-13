ec538d1167fd2ed6018cb1eb8fdfe87c4e0afb26
Peter Barry <pbarry@idbs.com>
2023-11-13 16:58:03 +0000

    s

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -9,16 +9,6 @@ $(document).ready(function() {
 function populateTrains() {
   var trainsContent = '';
 
-  $.getJSON( '/trains', function( data ) {
-
-  	trains = data
-
-    $.each(data, function(index){
-      trainsContent += '<div class="p-2"><a href="#" class="linkshowtrain" rel="' + index + '">' + this.name + '</a></div>';
-    });
-    $('#trains').html(trainsContent);
-    $('#trains').on('click', 'div a.linkshowtrain', showTrainInfo);
-  });
 };
 
 function showTrainInfo(event) {
--- routes/trains.js
+++ routes/trains.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var low = require('lowdb')
 var FileSync = require('lowdb/adapters/FileSync')
-
+/*
 var adapter = new FileSync('/opt/train-schedule/data/trains.json')
 var db = low(adapter)
 
-/* GET trains listing. */
+ GET trains listing. 
 router.get('/', function(req, res, next) {
   res.send(db.get('trains').sortBy('name').value());
-});
+});*/
 
 module.exports = router;
